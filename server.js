0c921dcd1a15723c0d08df0698ec9e6c98b29578
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2022-11-19 09:58:52 +0300

    connected database to express

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
+
+const DB = process.env.DATABASE_URL.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log('DB connection successful');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
