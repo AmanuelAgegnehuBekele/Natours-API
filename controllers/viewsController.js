e0cf743ae1df37379b416c8f37643f34da26ba7c
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2023-01-02 16:50:07 +0300

    designed pug template

---
+++ controllers/viewsController.js
@@ -0,0 +1,22 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  const tours = await Tour.find();
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours,
+  });
+});
+
+exports.getTour = catchAsync(async (req, res, next) => {
+  slug = req.params.slug;
+  const tour = await Tour.findOne({ slug: slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
+  });
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour,
+  });
+});
