86c585e559ad5bb3ada73d27ca9368157834021b
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2023-02-20 10:48:31 +0300

    added welcome and forget password email

--- utils/email.js
+++ utils/email.js
@@ -1,23 +1,61 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.form = `Amanuel Agegnehu <${process.env.EMAIL_FROM}>`;
+  }
 
-  const mailOptions = {
-    from: 'Amanuel Agegnehu <amanuelagegnehu2@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      //Sendgrid
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
 
-  await transporter.sendMail(mailOptions);
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    const mailOptions = {
+      from: this.form,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html, {
+        wordwrap: false,
+      }),
+    };
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token(valid for only 10 minutes)'
+    );
+  }
 };
-
-module.exports = sendEmail;
