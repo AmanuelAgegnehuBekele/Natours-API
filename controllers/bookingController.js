4faf74f1c8bd035ab2e4a75913621f23fca97e1f
Amanuel Agegnehu Bekele <amanuelagegnehu2@gmail.com>
2023-03-29 21:05:42 +0300

    added images

---
+++ controllers/bookingController.js
@@ -0,0 +1,40 @@
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`);
+const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourId);
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    mode: 'payment',
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+        // name: `${tour.name} Tour`,
+        // description: tour.summary,
+        // images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        // amount: tour.price * 100,
+        // currency: 'usd',
+        quantity: 1,
+      },
+    ],
+  });
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
